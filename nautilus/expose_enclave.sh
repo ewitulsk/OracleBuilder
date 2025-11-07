9cb780074f9b4fa4ee1bb9d58b8d4a4f14d26e47
Evan Witulski <ewitulsk@calpoly.edu>
2025-11-06 19:43:37 -0800

    nautilus commit

---
+++ nautilus/expose_enclave.sh
@@ -0,0 +1,16 @@
+# Copyright (c), Mysten Labs, Inc.
+# SPDX-License-Identifier: Apache-2.0
+#!/bin/bash
+
+# Gets the enclave id and CID
+# expects there to be only one enclave running
+ENCLAVE_ID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
+ENCLAVE_CID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveCID")
+
+sleep 5
+# Secrets-block
+SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:502186568577:secret:weather-api-key-HFZ6nA --region us-east-1 | jq -r .SecretString)
+echo "$SECRET_VALUE" | jq -R '{"API_KEY": .}' > secrets.jsoncat secrets.json | socat - VSOCK-CONNECT:$ENCLAVE_CID:7777
+socat TCP4-LISTEN:3000,reuseaddr,fork VSOCK-CONNECT:$ENCLAVE_CID:3000 &
+
+# Additional port configurations will be added here by configure_enclave.sh if needed
